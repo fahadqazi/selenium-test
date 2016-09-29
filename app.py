dc2af2132057086adc21744ba616df3bbb3f048b
Fahad Qazi <fahadqazi@gmail.com>
2016-09-29 22:00:43 +0100

    first commit. app.py has basic functionality

---
+++ app.py
@@ -0,0 +1,26 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+chromedriver_path = "/Users/Fahad/Downloads/chromedriver"
+
+driver = webdriver.Chrome(chromedriver_path)
+driver.get("http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk")
+
+countries = ["Canada","Germany", "Iceland","Pakistan", "Singapore", "South Africa"]
+
+for country in countries:    
+    input_element = driver.find_element_by_id("countryName")
+    input_element.clear()
+    input_element.send_keys(country)
+    input_element.text
+    input_element.send_keys(Keys.RETURN)
+    element1 = WebDriverWait(driver,1).until(EC.element_to_be_clickable((By.ID, "paymonthly")))
+    element1.click()
+    cost = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, """//*[@id="standardRatesTable"]/tbody/tr[1]/td[2]""")))
+    print country + " ", cost.text
+    driver.refresh()
+
