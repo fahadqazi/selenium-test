5b6390e1cb0bcedab887c878f187b4468d44bba6
Fahad Qazi <fahadqazi@gmail.com>
2016-09-29 23:23:47 +0100

    added utf-8 encoding and testcase for returning the correct call cost value

--- test_app.py
+++ test_app.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class DataScrapApp(unittest.TestCase):
 
@@ -17,7 +21,12 @@ class DataScrapApp(unittest.TestCase):
         self.assertEquals("", input_element.get_attribute('value'))
         input_element.send_keys(country)
         self.assertEquals("Canada", input_element.get_attribute('value'))
-
+        input_element.send_keys(Keys.RETURN)
+        payMonthlyButton = WebDriverWait(driver,1).until(EC.element_to_be_clickable((By.ID, "paymonthly")))
+        payMonthlyButton.click()
+        callCost = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, """//*[@id="standardRatesTable"]/tbody/tr[1]/td[2]""")))
+        
+        self.assertEquals("£1.50", callCost.text.encode('utf-8'))
         
     def tearDown(self):
         self.driver.close()
